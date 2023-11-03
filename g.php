0a6c47e638e22c7e5871e2ee50d09b2fd05d2877
FreedomXploit <140694236+FreedomXploit@users.noreply.github.com>
2023-11-03 13:42:37 +0700

    Add files via upload

---
+++ g.php
@@ -0,0 +1,42 @@
+
+><html>
+<head>
+<title>HACKED BY ALIGATOR TEAM OPRATION</title>
+ <link rel="icon" href="https://kosred.com/a/ggmgqy.jpg" type="image/x-icon"/>
+ <meta name="description" content="#IndonesianHackerRulez">
+ <link href="https://fonts.googleapis.com/css2?family=Oxygen" rel="stylesheet">
+</head>
+<body>
+<style>
+html {
+	background-color: black;
+	color: #000;
+}
+h2 {
+	font-family:"Bold 700 Italic",Mali;
+	color:red;
+}
+h3, h4 {
+	font-family:"Oxygen",serif;
+	color:white;
+}
+a {
+	color: white;
+	text-decoration: none;
+}
+::selection {
+	color:white;
+	background:#000;
+}
+</style>
+<table width="100%" height="100%">
+<td align="center">
+<img alt="#FUCK" src="https://kosred.com/a/ggmgqy.jpg" width="300px">
+ <h2>AGEND 0A2786 | FSITE-CODE </h2>
+  <h4> WE ARE ALIGATOR <br><br>BERANILAH MENUNTUT RAGA DAN MENTAL JIKA MEMANG MEMILIKI KAPASITAS.<br>BERKEINGINAN MEMILIKI LANGIT, MAKA JUGA HARUS BERKEINGINAN GELAP, GEMURU, PETIR SERTA BADAINYA</ins><br></h4>
+  <h2> <a href="https://t.me/aligator_Op" > #OpMalaysia</a></h2>
+  <audio controls="controls" src="https://kosred.com/a/snrhi.mp3"></audio>
+ <h4>- <font color="white">Greetz</font> -<br>DrVee_404 | AnggaXploit | Iman19X | ZALCYBER_EXPLOIT | IzunaSec666 | MastaBeen | 50ftex | M4sB0y | Fakesite |Network Seven | y | ./FreedomXploit</h4>
+<h3>- <font color="red">We are</font> -<br>-{ ALIGATOR BLACKHAT OPRATION }-</h3>
+</body>
+</html>
